d74f50937f01d0c6c2e08eada24d00c1544b1f44
saraAlsayd <roraya2442@gmail.com>
2023-11-28 09:02:29 -0800

    upp1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
 # install flask from pip3 by use  Puppet,
 
-# install_flask.pp
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
+package {'puppet-lint':
+  ensure => '2.1.0',
+  provider => 'gem',
 }
