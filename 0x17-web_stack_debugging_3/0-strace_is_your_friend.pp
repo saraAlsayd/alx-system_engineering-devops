97a880da68639a4156da830cbdc2315f231521e6
saraAlsayd <roraya2442@gmail.com>
2024-02-16 15:40:14 +0000

    web

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #File: 0-strace_is_your_friend.pp
 exec { 'fix-wordpress':
- command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
- path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
