fb526c286fb905ca5bfccf235cb935a7a49da993
saraAlsayd <roraya2442@gmail.com>
2024-02-16 14:27:40 +0000

    up

---
+++ 0x16-api_advanced/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#fixes bad "phpp" extensions to "php" in "wp-settings.php".
+
+exec{'fix-wordpress':
+ command => 'sed -i s/phpp/g /var/www/html/wp-settings.php',
+ path    => '/usr/local/bin/:/bin/'
+}
