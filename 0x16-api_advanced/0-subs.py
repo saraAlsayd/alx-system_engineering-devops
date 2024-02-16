2b0b492ba550bb6078a23f65b9558bd97459e3c3
saraAlsayd <roraya2442@gmail.com>
2024-02-16 07:58:31 +0000

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-"""
-This module defines a function to query the Reddit API and return
-the number of subscribers for a given subreddit.
-"""
-import requests
+"""Module for task 0"""
+
 
 def number_of_subscribers(subreddit):
-	 """
-    Queries the Reddit API and returns the number of subscribers for
-    a given subreddit. If the subreddit is invalid, returns 0.
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    Args:
-        subreddit (str): The name of the subreddit.
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
 
-    Returns:
-        int: The number of subscribers or 0 if the subreddit is invalid.
-    """
-	#Define the Url for the subreddit's info
-	url = f"https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
-	#Set a Custom User-Agent Header to avoid error
-	headers = {'User-Agent': 'My_User_Agent'},
-	 allow_redirects=False)
-	#Set A Get Request to the Reddit API
-	response = requests.get(url, headers=headers)
-	#check the request
-	if response.status_code == 200:
-		#Parse The response of the JSON
-		data = response.JSON()
-		#Extract And Return
-		return data['data']['subscribers']
-	elif response.status_code == 404:
-		#subreddit not found return 0
-		return 0
+    return sub_info.json().get("data").get("subscribers")
